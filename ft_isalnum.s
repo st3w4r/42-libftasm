bd2ef8881507dfcb28979e170f6b4525b7e0a32b
Yanael Barbier <ybarbier@student.42.fr>
2015-04-14 23:28:03 +0200

    Send ft_isalnum

---
+++ ft_isalnum.s
@@ -0,0 +1,33 @@
+; **************************************************************************** ;
+;                                                                              ;
+;                                                         :::      ::::::::    ;
+;    ft_isalnum.s                                       :+:      :+:    :+:    ;
+;                                                     +:+ +:+         +:+      ;
+;    By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+         ;
+;                                                 +#+#+#+#+#+   +#+            ;
+;    Created: 2015/04/14 22:00:30 by ybarbier          #+#    #+#              ;
+;    Updated: 2015/04/14 22:00:32 by ybarbier         ###   ########.fr        ;
+;                                                                              ;
+; **************************************************************************** ;
+global _ft_isalnum
+extern _ft_isalpha
+extern _ft_isdigit
+
+section .text
+_ft_isalnum:
+	call _ft_isalpha
+	mov r12, rax
+	call _ft_isdigit
+	mov r13, rax
+	or r12, r13
+	cmp r12, 0
+	je not_alnum
+	mov rax, 1
+	jmp return
+
+not_alnum:
+	mov rax, 0
+	jmp return
+
+return:
+	ret
