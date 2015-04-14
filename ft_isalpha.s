8b4a98f26415b3a30c9ca325f5c46b210c3a470a
Yanael Barbier <ybarbier@student.42.fr>
2015-04-14 21:42:29 +0200

    Add ft_isalpha.s

--- ft_isalpha.s
+++ ft_isalpha.s
@@ -12,5 +12,29 @@
 
 global _ft_isalpha
 
-setion .text
-	mov rax, rdi
+section .text
+_ft_isalpha:
+	cmp rdi, 0x41	; Comparaison avec A
+	jl not_alpha	; Saut si inferrieur à A
+
+	cmp rdi, 0x7a	; Comparaison avec z
+	jg not_alpha	; Saut a Faux si superieur à Z
+
+	cmp rdi, 0x5a	; Comparaison avec Z
+	jle is_alpha	; Saut a vrai si inferieur ou egal a Z
+
+	cmp rdi, 0x61	; Comparaison avec a
+	jge is_alpha	; Saut a vrai si superieur ou egal a a
+
+	jmp not_alpha
+
+is_alpha:
+	mov rax, 1
+	jmp return
+
+not_alpha:
+	mov rax, 0
+	jmp return
+
+return:
+	ret
